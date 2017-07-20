6feaf6cdc3abe53fb8089cdb5211ad5673ccb747
Jason Collins <the.oem@gmail.com>
2017-07-19 21:50:04 -0600

    Setup KNEX files for DB creation and seed data.

---
+++ app/utils/constants.js
@@ -0,0 +1,4 @@
+const API_KEY = '?apiKey=45c67617f89144868dafde322f26d8e3';
+const APP_ID = '?appId=14178&callback=myCallback';
+const BASE_URL = 'http://addb.absolutdrinks.com/drinks/?apiKey=45c67617f89144868dafde322f26d8e3';
+const DRINKS_URL = `/drinks/?apiKey=45c67617f89144868dafde322f26d8e3`;
